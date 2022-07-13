e282b6a0d938b3b79ff2c4bbc3231c9f48b856b6
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-07-13 08:40:43 +0600

    display csv file with streamlit done

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
+import streamlit as st
 import pandas as pd
+import numpy as np
+from sklearn import datasets
 
+st.title('WATER QUALITY PREDICTION APP')
 df = pd.read_csv("water_potability.csv")
-df.head()
+# df.head()
+# st.write(df)
+# st.sidebar
+st.write(df.head())
+
+
+# df.shape # (3276, 10) row and column or feature and attribute
+
+# df.isnull().sum()
+
+df.describe()
