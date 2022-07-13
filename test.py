abab911d5a83920889514e43d4aae4de62fd4bdb
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-07-13 09:57:07 +0600

    sidebar menu done

--- test.py
+++ test.py
@@ -1,18 +1,30 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pandas as pd
 import numpy as np
 from sklearn import datasets
 
-st.title('WATER QUALITY PREDICTION APP')
+# st.title('WATER QUALITY PREDICTION APP')
 df = pd.read_csv("water_potability.csv")
 # df.head()
 # st.write(df)
 # st.sidebar
-st.write(df.head())
 
+with st.sidebar:
+    selected = option_menu(
+        # menu_title="Select Menu",
+        menu_title=None,
+        options=['Home', 'Dataset', 'Contact'],
+        icons=["droplet", 'bar-chart-line', 'clipboard-data']
+    )
 
-# df.shape # (3276, 10) row and column or feature and attribute
-
-# df.isnull().sum()
-
-df.describe()
+# if selected == 'Select Menu':
+#     st.title("Water Quality Prediction App")
+if selected == 'Home':
+    st.title('WATER QUALITY PREDICTION APP')
+if selected == "Dataset":
+    st.title(f'Details About {selected}')
+    dtHeadBtn = st.button("Show First 5 Rows of Dataset")
+# st.write(df.head())
+    if dtHeadBtn:
+        st.write(df.head())
