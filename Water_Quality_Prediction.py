4dfde89b6387ee5f16ab4b733ac14ef9c139d0eb
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-07-14 11:23:16 +0600

    comment some important point

--- Water_Quality_Prediction.py
+++ Water_Quality_Prediction.py
@@ -27,59 +27,78 @@ method does the replacing in the original DataFrame instead.
 df.isnull().sum()
 
 # how much 0 and how much 1
+# resampling data
 df.Potability.value_counts()
 # value_counts() function to count the frequency of unique values
 
-# df.Potability.value_counts().plot(kind ='pie')
-
+# visualize the potability using countplot() function of seaborn
 sns.countplot(df['Potability'])
 plt.show()
 
-sns.distplot(df['ph'])
+# display the ph value using the displot
+sns.distplot(df['ph']) # it's a normal distribution
 plt.show()
 
+# display the entire dataset using the hist method 
 df.hist(figsize=(14, 14))
 plt.show()
 
+# visualize the correlation of all the features using heatmap() function
+# of seaborn
 plt.figure(figsize=(13, 8))
 sns.heatmap(df.corr(), annot=True, cmap='terrain')
 plt.show()
+# we can see there is no correlation between any feature
+# so it means we cannot reduce the dimension
 
+# outliers using the boxplot() function
 df.boxplot(figsize=(14, 7))
 
-X = df.drop("Potability", axis=1)
-Y = df['Potability']
+# divide the dataset in independent and dependent features
+X = df.drop("Potability", axis=1) # X contains all the independent features except the potibility
+Y = df['Potability'] # Y contains target feature potability
 
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.2, random_state=101, shuffle=True)
+# split the dataset into training and testing using train_test_split() function
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=101, shuffle=True)
+# test_size is 20%
+# random_state is basically used to ignore the shuffle everytime
+# train_test_split() returns 4 datasets
 
-df = DecisionTreeClassifier(
-    criterion='gini', min_samples_split=10, splitter='best')
-df.fit(X_train, Y_train)
+# train the model using the decision tree classifier
+df = DecisionTreeClassifier(criterion='gini', min_samples_split=10, splitter='best') # define the DT model
+df.fit(X_train, Y_train) 
+# fit the model (it means learn the parameter from training data)
 
+# check the model that how it is performing on the test data
+# give the test data and check the accuracy on the test dataset and predicted data set
 prediction = df.predict(X_test)
 print(f'Accuracy Score = {accuracy_score(Y_test,prediction)*100}')
 print(f'Confusion Matrix = \n {confusion_matrix(Y_test,prediction)}')
-print(
-    f"Classification Report = \n {classification_report(Y_test, prediction)}")
+print(f"Classification Report = \n {classification_report(Y_test, prediction)}")
 
-res = df.predict([[5.735724, 158.318745, 25363.016594, 7.728601,
-                 377.543291, 568.304671, 13.626624, 75.952337, 4.732954]])[0]
+# predict a single row that how the model perform only one row
+res = df.predict([[5.735724, 158.318745, 25363.016594, 7.728601, 377.543291, 568.304671, 13.626624, 75.952337, 4.732954]])[0]
 res
 
 
-model = DecisionTreeClassifier()
+# apply Hyper Parameter Tuning on DT classifier
+model = DecisionTreeClassifier() # define DT Classifier
 criterion = ['gini', 'entropy']
 splitter = ['best', 'random']
 min_samples_split = [2, 4, 6, 8, 10, 12, 14]
+# criterion, splitter, min_samples_split are 3 hyper parameters
 
-grid = dict(splitter=splitter, criterion=criterion,
-            min_samples_split=min_samples_split)
-cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-grid_search_dt = GridSearchCV(
-    estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+# created a dictionary of these parameters
+grid = dict(splitter=splitter, criterion=criterion, min_samples_split=min_samples_split)
+# used repeated stratified k fold cross validation 
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1) # given 10 splits
+# define grid search cv which is basically used to perform hyper parameter tuning
+grid_search_dt = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+# train the model using the training dataset
 grid_search_dt.fit(X_train, Y_train)
 
+
+# test the model and check the accuracy
 print(
     f"Best: {grid_search_dt.best_score_ :.3f } using {grid_search_dt.best_params_}")
 means = grid_search_dt.cv_results_["mean_test_score"]
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# import all the required libraries to train the model or visualize the dataset
 from unicodedata import numeric
 import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu
@@ -11,26 +12,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-df = pd.read_csv("water_potability.csv")
+df = pd.read_csv("water_potability.csv")  # loading dataset
 
 with st.sidebar:
     selected = option_menu(
         # menu_title="Select Menu",
         menu_title=None,
-        options=['Home', 'Dataset', 'Papers'],
-        icons=["droplet", 'bar-chart-line', 'clipboard-data']
+        options=['Home', 'Dataset',
+                 'Dataset Visualization', 'About App', 'Papers'],
+        icons=["droplet", 'bar-chart-line',
+               'graph-up-arrow', 'clipboard-data', 'newspaper']
     )
 
 if selected == 'Home':
     st.title('WATER QUALITY PREDICTION APP')
 if selected == "Dataset":
-    st.title(f'Visualization of {selected} in Different Ways')
+    st.header(f'Visualization of {selected} in Different Ways')
+    st.markdown('--------------------')
 
-    dt_btn = st.button("Whole Dataset")
+    dt_btn = st.button("Display Whole Dataset")
     if dt_btn:
         st.dataframe(df)
 
-    dtHeadBtn = st.button("First Five Rows of Dataset")
+    dtHeadBtn = st.button("Display Top Five Rows of Dataset")
     if dtHeadBtn:
         st.write(df.head())
 
@@ -45,24 +49,53 @@ if selected == "Dataset":
 
     describe_btn = st.button("Describe Dataset")
     if describe_btn:
-        st.markdown('------------')
         st.write(df.describe())
 
-    potability_btn = st.button("Potability Rate")
+    potability_btn = st.button("Display the Potability")
     if potability_btn:
         st.write(df.Potability.value_counts())
 
-        # select_box = st.selectbox(label='Feature', options='numeric_columns')
-        # sns.displot(datas[select_box])
-        # st.pyplot()
+if selected == "Dataset Visualization":
+    st.markdown('')
+    # select_box = st.selectbox(label='Feature', options='numeric_columns')
+    # sns.displot(datas[select_box])
+    # st.pyplot()
 
-        # sns.countplot(df['Potability'])
-        # fig = plt.show()
-        # st.area_chart(data=fig, width=0, height=0, use_container_width=True)
+    # sns.countplot(df['Potability'])
+    # fig = plt.show()
+    # st.area_chart(data=fig, width=0, height=0, use_container_width=True)
 
+if selected == "About App":
+    st.header(f"Details {selected}")
+    st.markdown('##### Context: ')
+    st.markdown('Access to safe drinking-water is essential to health, basic human right and a component of effective policy for health protection. This is important as a health and developtment issue at a national, regional and local level. In some regions, it has been shown that investments in water supply and sanitation can yield a net economic benefit, since the reductions in adverse health effects and health care costs outweigh the costs of undertaking the intrventions.')
+    st.markdown('##### Content: ')
+    st.markdown(
+        'The water_potability.csv file contains water quality for 3276 different water bodies.')
+    st.markdown("##### 1. pH value:")
+    st.markdown('pH is an important parameter in evaluating the acid-base balance of water. It is also the indicator of acidic or alkaine condition of water status. WHO has recommended maximum limit of pH from 6.5 to 8.5. The current investigation ranges were 6.52-6.83 which are in the range of WHO standards.')
+    st.markdown('##### 2. Hardness:')
+    st.markdown('Hardness is mainly caused by calcium and magnesium salts. These salts are dissolbed from geologic deposits through which water travels. The length of time water is in contact with hardness producing material helps determine how much hardness there is in raw water. Hardness was originally defined as the capacity of water to preciptate soap caused by Calcium and Magnesium.')
+    st.markdown('##### 3. Solids (Total Dissolved Solids- TDS):')
+    st.markdown('Water has the ability to dissolbe a wide range of inorganic and some organc minerals or salts such an potassium, calcium, sodium, bicarbonates, chlorides, sulfate etc. These minerals produced unwanted taste and color in appearance of water. This is the important parameter for the use of water. The water with high TDS  value indicated that water is highly mineralized. Desirable limit for TDS  is 500 mg/l and maximum limit is 1000 mg/l which prescrived for drinking purpose.')
+    st.markdown('##### 4. Chloramines:')
+    st.markdown('Chlorine and chloramine are the major disinfectants used in public water systems. Chloramines are monst commonly formed when ammonia is added to chlorne to treat drinking waater. chlorine levels up to 4 mg per liter are condidered safe in drinking water.')
+    st.markdown('##### 5. Sulfate:')
+    st.markdown('Sulfates ar naturally occuring substances that are found in minerls, soil and rocks. They are present in ambient air, groundwater plants and food. The principal commercial use of sulfate is in the chemical industry. Sulfate concentration is seawater is about 2700 mg/L. It ranges from 3 to 30 mg/L in most freshwater suppplies, although mush higher concentrations(1000 mg/L) are found in some geographic locations.')
+    st.markdown('##### 6. Conductivity:')
+    st.markdown("Pure water is not a good conductor of electric current rather's a good insulator. Increase in icons concentreation enhances the electrical conductivity of water. Generally, the amount of dissolved solids in water determines the electrical conductivity. Electrical conductivity (EC) actually measures the iconic process of a solution that enables it to transmit current. According to WHO  standards, EC value should not exceeded 400 microS/cm.")
+    st.markdown('##### 7. Organic_carbon:')
+    st.markdown('Total Organic Carbon (TOC) in source waters comes from decaying natural organic matter (NOM) as well as synthetic sources. TOC is measure of the total amount of carbon in organic compounds in pure water. According to US EPA < 2mg/L as TOC in treated/drinking water, and < 4 mg/L in source water which is use for treatment.')
+    st.markdown('##### 8. Trihalomathanes:')
+    st.markdown('THMs are chemicals which may be found in water treated with chlorine. The concentration of THMs in drinking water varies according to the level of organic material in the water, the amount of chlorine required to treat the water, and the temperature of the water that is being treated. THM levels up to 80 ppm is considered safe in drinking water.')
+    st.markdown('##### 9. Turbidity:')
+    st.markdown('The turbidity of water depends on the quality of solid matter present in the suspended state. It is a measure of light emitting properties of water and the test is used to indicate the quality of waste discharge with respect to colloidal matter. The mean turbidity value obtained for Wondo Genel Campus (0.98 NTU) is lower than the WHO recommended value of 5.00 NTU.')
+    st.markdown('##### 10. Potability:')
+    st.markdown('It indicates if water is safe for human consumption where 1 means Potable and 0 means Not Potable. (0) Water is not safe for drink and (1) Water is safe to drink.')
 if selected == "Papers":
-    st.title(f"Read Different {selected} in Water Potability")
+    st.header(f"Read Different {selected} in Water Potability")
 
+# fill the null values using the mean value
 df.fillna(df.mean(), inplace=True)
 df.isnull().sum()
 
@@ -77,8 +110,10 @@ st.write(df.fit(X_train, Y_train))
 
 prediction = df.predict(X_test)
 st.write('Accuracy Score: ', accuracy_score(Y_test, prediction)*100)
-st.write('Confusion Matrix: \n', confusion_matrix(Y_test,prediction))
-st.write("Classification Report: \n\n", classification_report(Y_test, prediction))
+st.write('Confusion Matrix: \n', confusion_matrix(Y_test, prediction))
+st.write("Classification Report: \n\n",
+         classification_report(Y_test, prediction))
 
-res = df.predict([[5.735724, 158.318745, 25363.016594, 7.728601, 377.543291, 568.304671, 13.626624, 75.952337, 4.732954]])[0]
+res = df.predict([[5.735724, 158.318745, 25363.016594, 7.728601,
+                 377.543291, 568.304671, 13.626624, 75.952337, 4.732954]])[0]
 st.write('res: ', res)
